abb49986956898852270af106b6c6d56521eb71d
Laurent Sorber <laurent.sorber@gmail.com>
2022-01-21 23:56:10 +0100

    feat: more conservative GitHub Actions triggers

--- {{ cookiecutter.package_name|slugify }}/tests/test_version.py
+++
@@ -1,8 +0,0 @@
-"""Test package version."""
-
-from importlib.metadata import version
-
-
-def test_version() -> None:
-    """Test the package version."""
-    assert isinstance(version("{{ cookiecutter.package_name|slugify }}"), str)
