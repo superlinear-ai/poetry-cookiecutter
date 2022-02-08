bf9ba35571fcfdbf2cd053a9280d90b76bd2c20e
Tanguy Naets <tanguy@radix.ai>
2022-02-08 14:35:05 +0100

    feat: Add coloredlogs

--- {{ cookiecutter.package_name|slugify }}/src/{{ cookiecutter.package_name|slugify|replace("-", "_") }}/__init__.py
+++ {{ cookiecutter.package_name|slugify }}/src/{{ cookiecutter.package_name|slugify|replace("-", "_") }}/__init__.py
@@ -1,1 +1,27 @@
 """{{ cookiecutter.package_name }} package."""
+
+import logging
+
+import coloredlogs
+
+
+def configure_root_logger() -> None:
+    """Configure the root logger."""
+    # Remove all handlers associated with the root logger object.
+    for handler in logging.root.handlers:
+        logging.root.removeHandler(handler)
+    # Add coloredlogs' coloured StreamHandler to the root logger.
+    coloredlogs.install(
+        fmt="%(asctime)s %(name)s %(funcName)s %(levelname)s %(message)s",
+        datefmt="%H:%M:%S",
+        field_styles={
+            "asctime": {"color": "green"},
+            "name": {"color": "blue"},
+            "funcName": {"color": "magenta"},
+            "levelname": {"color": "black", "bold": True},
+        },
+        isatty=True,
+    )
+
+
+configure_root_logger()
