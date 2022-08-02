3d0cfac885c9e48803ae747f19c4487f349869c5
Laurent Sorber <laurent.sorber@gmail.com>
2022-08-02 12:01:47 +0200

    feat: make conditional files portable

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+# Read Cookiecutter configuration.
+with_fastapi_api = int("{{ cookiecutter.with_fastapi_api }}")
+with_sentry_logging = int("{{ cookiecutter.with_sentry_logging }}")
+with_streamlit_app = int("{{ cookiecutter.with_streamlit_app }}")
+with_typer_cli = int("{{ cookiecutter.with_typer_cli }}")
+continuous_integration = "{{ cookiecutter.continuous_integration }}"
+is_deployable_app = "{{ cookiecutter.with_fastapi_api|int or cookiecutter.with_streamlit_app|int }}" == "True"
+is_publishable_package = "{{ not cookiecutter.with_fastapi_api|int and not cookiecutter.with_streamlit_app|int }}" == "True"
+
+# Remove FastAPI if not selected.
+if not with_fastapi_api:
+    os.remove("src/api.py")
+    os.remove("tests/test_api.py")
+
+# Remove Sentry if not selected.
+if not with_sentry_logging:
+    os.remove("src/sentry.py")
+    os.remove("tests/test_sentry.py")
+
+# Remove Streamlit if not selected.
+if not with_streamlit_app:
+    os.remove("streamlit_app.py")
+
+# Remove Typer if not selected.
+if not with_typer_cli:
+    os.remove("src/cli.py")
+    os.remove("tests/test_cli.py")
+
+# Remove the continuous integration provider that is not selected.
+if continuous_integration != "GitHub":
+    shutil.rmtree(".github/")
+elif continuous_integration != "GitLab":
+    os.remove(".gitlab-ci.yml")
+
+# Remove unused GitHub Actions workflows.
+if continuous_integration == "GitHub":
+    if not is_deployable_app:
+        os.remove(".github/workflows/deploy.yml")
+    if not is_publishable_package:
+        os.remove(".github/workflows/publish.yml")
--- hooks/post_gen_project.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Remove .github/ if GitHub is not the selected continuous integration provider.
-if [ "{{ cookiecutter.continuous_integration }}" != "GitHub" ]
-then
-    rm -rf .github/
-fi
