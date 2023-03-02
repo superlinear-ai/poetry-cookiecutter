79aea2b34ab8f301a6058c37d826f12013b8523f
Laurent Sorber <laurent.sorber@gmail.com>
2023-03-02 11:06:51 +0100

    fix: address PLR2004 and add tests (#177)

--- {{ cookiecutter.__package_name_kebab_case }}/tests/test_api.py
+++ {{ cookiecutter.__package_name_kebab_case }}/tests/test_api.py
@@ -1,5 +1,6 @@
 """Test {{ cookiecutter.package_name }} REST API."""
 
+import httpx
 from fastapi.testclient import TestClient
 
 from {{ cookiecutter.__package_name_snake_case }}.api import app
@@ -10,4 +11,4 @@ client = TestClient(app)
 def test_read_root() -> None:
     """Test that reading the root is successful."""
     response = client.get("/")
-    assert response.status_code == 200
+    assert httpx.codes.is_success(response.status_code)
