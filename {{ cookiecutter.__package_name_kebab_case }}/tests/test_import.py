6d7373100acca9c8099bfaea972bcb6f2aa92933
Laurent Sorber <laurent.sorber@gmail.com>
2023-03-02 09:50:50 +0000

    docs: update test description

--- {{ cookiecutter.__package_name_kebab_case }}/tests/test_import.py
+++ {{ cookiecutter.__package_name_kebab_case }}/tests/test_import.py
@@ -4,5 +4,5 @@ import {{ cookiecutter.__package_name_snake_case }}
 
 
 def test_import() -> None:
-    """Test that the say command works as expected."""
+    """Test that the package can be imported."""
     assert isinstance({{ cookiecutter.__package_name_snake_case }}.__name__, str)
