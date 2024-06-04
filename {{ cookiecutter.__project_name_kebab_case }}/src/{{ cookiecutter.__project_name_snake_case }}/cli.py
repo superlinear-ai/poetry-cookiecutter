5137d5f9f38fbc51037625d1d214b4a0e42790a0
Olivier Belhumeur <46036275+GameSetAndMatch@users.noreply.github.com>
2024-06-04 08:18:35 -0400

    Update cli.py
    fix(cli.py): fixed ruff format

--- {{ cookiecutter.__project_name_kebab_case }}/src/{{ cookiecutter.__project_name_snake_case }}/cli.py
+++ {{ cookiecutter.__project_name_kebab_case }}/src/{{ cookiecutter.__project_name_snake_case }}/cli.py
@@ -10,5 +10,4 @@ app = typer.Typer()
 @app.command()
 def fire(name: str = "Chell") -> None:
     """Fire portal gun."""
-    print(
-        f"[bold red]Alert![/bold red] {name} fired [green]portal gun[/green] :boom:")
+    print(f"[bold red]Alert![/bold red] {name} fired [green]portal gun[/green] :boom:")
